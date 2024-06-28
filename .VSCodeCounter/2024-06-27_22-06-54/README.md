791d6a7e10cb1b7909509fad669540c387eef083
Felipe S.C. Lorentz <SCLorentz@users.noreply.github.com>
2024-06-27 22:08:13 -0300

    routes

---
+++ .VSCodeCounter/2024-06-27_22-06-54/README.md
@@ -0,0 +1,26 @@
+# Summary
+
+Date : 2024-06-27 22:06:54
+
+Directory /home/sclorentz/Documentos/github/rok-page
+
+Total : 6 files,  100 codes, 6 comments, 22 blanks, all 128 lines
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| HTML | 3 | 47 | 0 | 2 | 49 |
+| Rust | 2 | 43 | 6 | 12 | 61 |
+| Markdown | 1 | 10 | 0 | 8 | 18 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 6 | 100 | 6 | 22 | 128 |
+| . (Files) | 1 | 10 | 0 | 8 | 18 |
+| src | 2 | 43 | 6 | 12 | 61 |
+| static | 3 | 47 | 0 | 2 | 49 |
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,50 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
-use actix_files::NamedFile;
-use actix_web::HttpRequest;
-use std::path::PathBuf;
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
-#[get("/")]
-async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
-    let path: PathBuf = req.match_info().query("index").parse().unwrap();
-    Ok(NamedFile::open(path)?)
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let mut status_line = "HTTP/1.1 200 OK";
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+    let mut request = |response: &str| {
+        let contents = fs::read_to_string(response).unwrap();
+        let length = contents.len();
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(index)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+        let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status_line, length, contents);
+
+        stream.write_all(response.as_bytes()).unwrap();
+    };
+
+    //println!("{:?}", request_line);
+    
+    match request_line {
+        c if c == "GET / HTTP/1.1" => request("./static/index.html"),
+        c if c == "GET /download HTTP/1.1" => request("./static/download.html"),
+        c if c == "GET /pkg/rok_page.js HTTP/1.1" => request("./pkg/rok_page_bg.js"),
+        c if c == "GET /pkg/rok_page_bg.wasm HTTP/1.1" => request("./pkg/rok_page_bg.wasm"),
+        _ => {
+            status_line = "HTTP/1.1 404 NOT FOUND";
+            let contents = fs::read_to_string("./static/404.html").unwrap();
+            let length = contents.len();
+
+            let response = format!(
+                "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+            );
+
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    }
 }
